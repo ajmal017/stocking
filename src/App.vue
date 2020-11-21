a9504f4d51d3c7445961c07b6b9a698ccd22f757
zell1880 <lzhao1880@gmail.com>
2020-11-21 18:28:54 +0800

    remove about page

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,6 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" to="/about">About</router-link>
-          </li>
-          <li class="nav-item">
             <router-link class="nav-link active" to="/survey">Survey</router-link>
           </li>
           <li class="nav-item">
